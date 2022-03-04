2313e1678ccf9090351e1ea738600a07a955b7a7
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-03-04 12:53:09 +0600

    destructure props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
-function Card(props) {
+function Card({
+  imageUrl,
+  location,
+  googleMapsUrl,
+  title,
+  startDate,
+  endDate,
+  description,
+}) {
   return (
     <div className="flex justify-center my-5">
       <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
         <img
           className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-          src={props.imageUrl}
+          src={imageUrl}
           alt=""
         />
         <div className="p-6 flex flex-col justify-start">
@@ -15,11 +23,11 @@ function Card(props) {
             <div className="flex items-center">
               <FaMapMarkerAlt className="text-red-500" />
               <p className="ml-2 tracking-[0.17em] text-xs">
-                {props.location.toUpperCase()}
+                {location.toUpperCase()}
               </p>
             </div>
             <a
-              href={props.googleMapsUrl}
+              href={googleMapsUrl}
               target="_blank"
               rel="noopener noreferrer"
               className="text-gray-400 underline text-xs"
@@ -27,13 +35,11 @@ function Card(props) {
               View on Google Maps
             </a>
           </div>
-          <h5 className="text-gray-900 text-2xl font-bold mb-2">
-            {props.title}
-          </h5>
+          <h5 className="text-gray-900 text-2xl font-bold mb-2">{title}</h5>
           <p className="text-gray-900 font-bold text-xs my-3">
-            {props.startDate} - {props.endDate}
+            {startDate} - {endDate}
           </p>
-          <p className="text-gray-900 text-xs mb-4">{props.description}</p>
+          <p className="text-gray-900 text-xs mb-4">{description}</p>
         </div>
       </div>
     </div>
